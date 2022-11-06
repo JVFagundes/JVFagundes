bc3d3aff3ca4dbdb290680d7e358a8609ddf8f8b
JVFagundes <117388140+JVFagundes@users.noreply.github.com>
2022-11-06 00:12:35 -0300

    Create sobre.php

---
+++ sobre.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   
+    <title>BocaScan</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" 
+    rel="stylesheet">
+
+</head>
+<body>
+
+<div class="wrapper">
+
+        <nav class="navbar">
+            
+            <ul>
+                <li><a class="active" href="index.php">Home</a></li>
+                <li><a class="active" href="manga.php">Mangás</a></li>
+                <li><a class="active" href="manhua.php">Manhuas</a></li>
+                <li><a class="active" href="manhwa.php">Manhwas</a></li>
+                <li><a class="active" href="sobre.php">Sobre</a></li>
+            </ul>
+    
+        </nav>
+
+        <div class="center">
+            <h1>BocaScan</h1>
+            <h2>EM BREVE!!!</h2>
+            </body>
+
+            <img class="thumb-img" src="bocaScan 1.jpeg"/>
+
+    </div>
+<style>
+
+*{
+    margin: 0;
+    padding: 0;
+}
+
+body{
+    background-color: #1C1C1C;
+}
+ 
+.navbar{
+    width: 100%;
+    height: 80px;
+    background: #7FFFD4;
+}    
+
+.navbar ul{
+    float: right;
+    margin-right: 20px;
+}
+
+.navbar ul li{
+    list-style: none;
+    margin: 0 8px;
+    display: inline-block;
+    line-height: 80px;
+}
+
+.navbar ul li a{
+    text-decoration: none;
+    color: #1C1C1C;
+    font-size: 20px;
+    font-family: 'Russo One', sans-serif;
+    padding: 6px 13px;
+    transition: .4s;
+}
+
+.navbar ul li a.active{
+    background: white;
+    border-radius: 2px;
+}
+
+.logo{
+    width: 80px;
+    height: 80px;
+    padding: 0 0px;
+}
+
+.wrapper .center{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-family:'Russo One', sans-serif;
+    user-select: none;
+}
+
+.center{
+    margin-top: -7%;
+    margin-left: 0%;
+}
+.center h1{
+    color: #7FFFD4;
+    font-size: 70px;
+    font-weight: bold;
+    width: 900px;
+    text-align: center;
+}
+
+.center h2{
+    color: #7FFFD4;
+    font-size: 70px;
+    font-weight: bold;
+    width: 850px;
+    margin-top: 10px;
+    margin-left: 20px;
+    text-align: center;
+}
+
+.center .buttons{
+    margin: 30px 270px;
+}
+
+.buttons button{
+    height: 70px;
+    width: 170px;
+    font-size: 22px;
+    font-weight: bold;
+    color: white;
+    background-color: red;
+    border: 5px solid #cc0000;
+    outline: none;
+    cursor: pointer;
+    transition: .5s;
+}
+
+
+
+.buttons button:hover{
+    background-color: #cc0000;
+}
+
+a{
+    text-decoration: none;
+}
+
+.thumb-img{
+	border-radius: 10px;
+    height: 200%;
+    object-fit: fill;
+    object-position: top;
+    position: absolute;
+    width: 50%;
+    transition: 0.2s ease-in-out;
+    left: 24%;
+}
+
+.thumb-img:hover{
+	transform: scale(1.5);
+}
+
+</style>
+
+</html>
